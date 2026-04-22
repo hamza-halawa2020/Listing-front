165da1cae803991ed402dc69982ad195bb6a896f
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-04-22 13:20:25 +0200

    up

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    backEndUrl: 'https://dashboard.careandshare-eg.com/api',
-    imgUrl: 'https://dashboard.careandshare-eg.com',
-    // backEndUrl: 'http://listing_dashboard.test/api',
-    // imgUrl: 'http://listing_dashboard.test',
+// backEndUrl: 'https://dashboard.careandshare-eg.com/api',
+// imgUrl: 'https://dashboard.careandshare-eg.com',
+    backEndUrl: 'http://listing_dashboard.test/api',
+    imgUrl: 'http://listing_dashboard.test',
 };
 
 
