cd27fe514553941897c07189e01a2654566d6bc5
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-04-23 12:36:16 +0200

    up

--- src/app/common/whatsapp-float/whatsapp-float.component.ts
+++ src/app/common/whatsapp-float/whatsapp-float.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SettingService, Settings } from '../../shared/services/setting.service';
+import { SettingService } from '../../shared/services/setting.service';
 import { Subscription } from 'rxjs';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
